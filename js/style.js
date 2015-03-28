834d7a49a181a3db699cbeaa00256dff61a5b8c5
Max Yeo <mzwyeo@gmail.com>
2015-03-28 02:58:37 -0400

    redesigned, added contact links

---
+++ js/style.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {  
+    sizeitup();
+});
+
+$(window).resize(function() {
+  sizeitup();
+});
+
+function sizeitup() {
+  var bodyh = $(window).height() * .9;
+  var bodyw = $(window).width();
+  if (bodyw > 610) {
+    $(".top").css("height",bodyh);
+  } else {
+    $(".top").css("height","inherit");
+  }
+}
